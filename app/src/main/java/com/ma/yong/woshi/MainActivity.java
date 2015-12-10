2f9eb7c483d1b91cc3c4309b89acabfbd3685342
mayong3838 <1107324722@qq.com>
2015-12-10 13:58:58 +0800

    jkkghj

--- app/src/main/java/com/ma/yong/woshi/MainActivity.java
+++ app/src/main/java/com/ma/yong/woshi/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //gsgalhkhjkgjgljkkkkljjkglj,hffbssgfahhaff
+    //kgjs;lkghj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
